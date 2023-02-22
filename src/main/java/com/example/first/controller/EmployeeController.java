8ea865759575571435e02bbc98f5db2e400812f0
Prashant <prashant.shingade@1clickcapital.com>
2023-02-22 16:49:25 +0530

    delete API Added

--- src/main/java/com/example/first/controller/EmployeeController.java
+++ src/main/java/com/example/first/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.first.exception.ResourceNotFoundException;
 import com.example.first.model.Employee;
 import com.example.first.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -57,4 +58,14 @@ public class EmployeeController {
 
     }
 
+
+    //build delete employee REST API
+    @DeleteMapping({"id"})
+    public ResponseEntity<HttpStatus> deleteEmployee(long id){
+         Employee employee=employeeRepository.findById(id)
+                 .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id"+id));
+
+         employeeRepository.delete(employee);
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
