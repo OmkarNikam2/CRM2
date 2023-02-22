24a8f39eccb5e7e15a52f3d4ba8e818b543f3bb9
Prashant <prashant.shingade@1clickcapital.com>
2023-02-22 16:25:24 +0530

    commit

--- src/main/java/com/example/first/controller/EmployeeController.java
+++ src/main/java/com/example/first/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.first.controller;
 
+import com.example.first.exception.ResourceNotFoundException;
 import com.example.first.model.Employee;
 import com.example.first.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
-
      @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -29,8 +30,31 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+
+
     //build get employee by id using REST API
+    @GetMapping("{id}")
+     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+         Employee employee= employeeRepository.findById(id)
+                 .orElseThrow(() -> new ResourceNotFoundException("employee not exist with id:"+ id));
+         return ResponseEntity.ok(employee);
+     }
 
 
+     //build update employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,@RequestBody Employee employeeDetails){
+         Employee updateEmployee = employeeRepository.findById(id)
+                 .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id:" +id));
+
+         updateEmployee.setFirstName(employeeDetails.getFirstName());
+         updateEmployee.setLastName(employeeDetails.getEmailId());
+         updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+         employeeRepository.save(updateEmployee);
+
+         return ResponseEntity.ok(updateEmployee);
+
+    }
 
 }
